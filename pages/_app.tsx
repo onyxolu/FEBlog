a5e38b6a97793012fdbcf9be64d5db2398243160
Olumide Ogundare <ogundare oloumide@gmail.com>
2024-03-25 12:51:30 +0000

    disabling lint for app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable */
+
 import "../styles.css";
 import "nextra-theme-docs/style.css";
 import "../custom.css";
 import { SSRProvider } from "@react-aria/ssr";
 
-// Shim requestIdleCallback in Safari
+// Shim requestIdleCallback in Safarigit add 
 if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
   window.requestIdleCallback = (fn) => setTimeout(fn, 1);
   window.cancelIdleCallback = (e) => clearTimeout(e);
